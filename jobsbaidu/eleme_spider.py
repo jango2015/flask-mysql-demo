1940364db077077b8276c0cffcee1778a29a8a66
jango2015 <jango2015>
2016-09-29 18:26:03 +0800

    add ele.me  restaurant spider

---
+++ jobsbaidu/eleme_spider.py
@@ -0,0 +1,8 @@
+
+eleme_cities ='https://mainsite-restapi.ele.me/shopping/v1/cities'
+
+restaurant_category ='https://mainsite-restapi.ele.me/shopping/v2/restaurant/category?latitude=31.21551&longitude=121.44695' #by gps
+
+pois_nearby ='https://mainsite-restapi.ele.me/v2/pois?extras%5B%5D=count&geohash=wtw3sjq6n6um&keyword=世纪大道&limit=20&type=nearby'
+
+restaurant_gps_nearby ='https://mainsite-restapi.ele.me/shopping/restaurants?extras%5B%5D=activities&geohash=wtw3syjydf6&latitude=31.23526&limit=24&longitude=121.50582&offset=0'  #offset : pagesize geohash: geo hashcode
